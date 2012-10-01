f1720d6274f7502a3de714b489857590aad01d49
Yehuda Sadeh <yehuda@inktank.com>
2012-10-01 12:37:14 -0700

    rgw: more explicit header_length calculation
    less hackish, clearer
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -346,7 +346,7 @@ int RGWPostObj_REST_S3::get_form_head()
   if (content_length == 0)
     return -ENODATA;
 
-  header_length = -s->bytes_received;
+  size_t start_receive = s->bytes_received;
   
   // get the part boundary
   string content_string = s->env->get("CONTENT_TYPE");
@@ -452,7 +452,7 @@ int RGWPostObj_REST_S3::get_form_head()
     }
   } while (!data_pending);
 
-  header_length += s->bytes_received;
+  header_length += (s->bytes_received - start_receive);
   content_length -= header_length;
 
   free(buf);
